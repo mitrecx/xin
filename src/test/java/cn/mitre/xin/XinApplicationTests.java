66282c71f3bfa82355fc9f8eb452d188a654e539
chenxing <mitrecx@163.com>
2021-12-10 00:22:25 +0800

    init

---
+++ src/test/java/cn/mitre/xin/XinApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mitre.xin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
